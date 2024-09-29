ff818bdc61fbebb60ad070426766ad542c3436fc
Devin Checa <devincheca@gmail.com>
2024-09-29 15:16:57 -0700

    Set up parcel

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  console.log('from app');
+  return (
+    <div>
+      <h1>Hello, React with Parcel.js</h1>
+    </div>
+  );
+}
+
+export default App;
+
